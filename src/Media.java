e703f0b5fa521876056c96fbad66e99c25ee78cd
EDUTEC <EDUTEC@10.8.0.7>
2013-06-15 08:45:27 -0300

    ok

---
+++ src/Media.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author EDUTEC
+ */
+public class Media {
+    public static void main(String[] args){
+        String nota2, nota3;
+        float nota1, media;
+        
+        nota1 = Integer.ParserFloat(JOptionPane.showInputDialog(null, "Informe a nota 1:"));            
+        nota2 = JOptionPane.showInputDialog(null, "Informe a nota 2:");  
+        nota3 = JOptionPane.showInputDialog(null, "Informe a nota 3:"); 
+        
+        
+    }
+ }
+    
---
+++ src/aula/pkg15/pkg06/pkg2013/Aula15062013.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula.pkg15.pkg06.pkg2013;
+
+/**
+ *
+ * @author EDUTEC
+ */
+public class Aula15062013 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
