cad541f3c7c6b9ab5429ca4a95d756a693a45532
EDUTEC <EDUTEC@10.8.0.7>
2013-06-15 09:04:45 -0300

    ok

--- src/Media.java
+++ src/Media.java
@@ -16,10 +16,16 @@ public class Media {
         float nota1,nota2, nota3, media;
         
         nota1 = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe a nota 1:"));            
-        nota2 = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe a nota 2:");  
-        nota3 = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe a nota 3:"); 
+        nota2 = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe a nota 2:"));  
+        nota3 = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe a nota 3:")); 
         
-        media = (nota1,nota2, nota3) / 3;
+        media = (nota1 + nota2 + nota3) / 3;
+                
+        if (media > 60){
+            System.out.println("Media = " + media + " :-)");
+        } else
+            System.out.println("Media = " + media);
+              
         
         
     }
