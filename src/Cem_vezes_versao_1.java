3f13228255f96d31714c1dde461bee0d6b85def2
EDUTEC <EDUTEC@10.8.0.7>
2013-06-15 10:30:19 -0300

    ok

--- src/Cem_vezes_versao_1.java
+++ src/Cem_vezes_versao_1.java
@@ -16,6 +16,7 @@ public class Cem_vezes_versao_1 {
         String nome = JOptionPane.showInputDialog("Informe sue nome!");
         for ( int cont = 0; cont < 100; cont ++){
             System.out.println(cont + 1 + " - " + nome );
+            
         }
     }
     
