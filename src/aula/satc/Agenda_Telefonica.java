23bf54ecfb40315d89a04bd358b3ab3a091fc956
EDUTEC <EDUTEC@10.8.0.7>
2013-06-15 11:48:46 -0300

    ok

---
+++ src/aula/satc/Agenda_Telefonica.java
@@ -0,0 +1,11 @@
+
+package aula.satc;
+
+public class Agenda_Telefonica {
+    public static void main(String[] args){
+        
+        String entry = {{"Florence", "735-1234", "Manila"},{"Joyce", "983-3333","Quezon City"},{"Becca", "456-3322","Manila"}};
+        
+    }
+    
+}
